8fda730fa498af16e9bb0e97d2483c6f84519347
SvveetMavis <107206864+SvveetMavis@users.noreply.github.com>
2022-06-17 16:14:43 -0400

    Create sloth.lua

---
+++ lua/terrortown/lang/en/sloth.lua
@@ -0,0 +1,14 @@
+local L = LANG.GetLanguageTableReference("en")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SLOTH.name] = "Sloth"
+L[SLOTH.defaultTeam] = "Team Sloths"
+L["hilite_win_" .. SLOTH.defaultTeam] = "TEAM SLOTH WON"
+L["win_" .. SLOTH.defaultTeam] = "The Sloth has won!"
+L["info_popup_" .. SLOTH.name] = [[Kill.]]
+L["body_found_" .. SLOTH.abbr] = "They were The Sloth!"
+L["search_role_" .. SLOTH.abbr] = "This person was The Sloth!"
+L["ev_win_" .. SLOTH.defaultTeam] = "The deadly Sloth won the round!"
+L["target_" .. SLOTH.name] = "Sloth"
+L["ttt2_desc_" .. SLOTH.name] = [[The Serialkiller needs to kill every player to win, but does is unable to die and can't stall the round]]
+
